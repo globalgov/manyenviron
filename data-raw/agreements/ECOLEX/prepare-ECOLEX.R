86f26ef0daff0d79b5faec1cd6889ce3454578e1
James Hollway <james.hollway@graduateinstitute.ch>
2021-01-25 11:18:21 +0100

    Started import of ECOLEX agreement data

---
+++ data-raw/agreements/ECOLEX/prepare-ECOLEX.R
@@ -0,0 +1,33 @@
+# ECOLEX Preparation Script
+
+# This is a template for importing, cleaning, and exporting data
+# ready for the qPackage.
+library(qData)
+
+# Stage one: Collecting data
+load("data-raw/agreements/ECOLEX/ecoagree.RData")
+ECOLEX <- eco_agree
+retain(c("ECOLEX"))
+
+# Stage two: Correcting data
+# In this stage you will want to correct the variable names and
+# formats of the 'ECOLEX' object until the object created
+# below (in stage three) passes all the tests. 
+ECOLEX <- as_tibble(ECOLEX) %>%
+  transmutate(ID = EcolexID,
+              Beg = standardise_dates(Date)) %>%
+  dplyr::arrange(Beg, ID)
+# qData includes several functions that should help cleaning and standardising your data.
+# Please see the vignettes or website for more details.
+
+# Stage three: Connecting data
+# Next run the following line to make ECOLEX available within the qPackage.
+export_data(ECOLEX, database = "agreements")
+# This function also does two additional things.
+# First, it creates a set of tests for this object to ensure adherence to certain standards.
+# You can hit Cmd-Shift-T (Mac) or Ctrl-Shift-T (Windows) to run these tests locally at any point.
+# Any test failures should be pretty self-explanatory and may require you to return
+# to stage two and further clean, standardise, or wrangle your data into the expected format.
+# Second, it also creates a documentation file for you to fill in.
+# Please make sure that you cite any sources appropriately and fill in as much detail
+# about the variables etc as possible.
