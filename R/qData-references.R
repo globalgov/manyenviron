1372cf5651bb8b7b89ae97e784bfa10349f114ce
James Hollway <james.hollway@graduateinstitute.ch>
2021-08-02 18:00:08 +0200

    Re #22 changed REF dataset in references database to ECOLEX

---
+++ R/qData-references.R
@@ -0,0 +1,11 @@
+#' references database documentation
+#'
+#' @format The references database is a list that contains the
+#' following 1 datasets: REF.
+#' For more information and references to each of the datasets used,
+#' please use the `data_source()`, `data_contrast()`, and `data_evolution()`
+#' functions.
+#'\describe{
+#' \item{REF: }{A dataset with 1164 observations and the following 3 variables: qID1, RefType, qID2.}
+#' }
+ "references"
