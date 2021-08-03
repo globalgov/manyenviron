290a3665a93aff0dc4f67e2c8f34db86283607a0
EstherPeev <esther.peev@graduateinstitute.ch>
2021-08-03 08:12:29 +0200

    Updated database documentation

--- R/qEnviron-references.R
+++ R/qEnviron-references.R
@@ -1,12 +1,14 @@
 #' references database documentation
 #'
 #' @format The references database is a list that contains the
-#' following 1 datasets: ECOLEX.
+#' following 2 datasets: ECOLEX, ECOLEX_REF.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()`, `data_contrast()`, and `data_evolution()`
 #' functions.
 #'\describe{
 #' \item{ECOLEX: }{A dataset with 1164 observations and the following
 #' 3 variables: qID1, RefType, qID2.}
+#' \item{ECOLEX_REF: }{A dataset with 1164 observations and the following
+#' 3 variables: Treaty1, RefType, Treaty2.}
 #' }
  "references"
