1e25e07399c6648308ffe774e51d33ea63c04515
EstherPeev <esther.peev@graduateinstitute.ch>
2021-09-03 17:57:36 +0200

    Deleted package argument from export_data() function

--- data-raw/agreements/IEADB/prepare-IEADB.R
+++ data-raw/agreements/IEADB/prepare-IEADB.R
@@ -33,7 +33,7 @@ IEADB$qID <- qCreate::code_agreements(IEADB, IEADB$Title, IEADB$Beg)
 
 # Stage three: Connecting data
 # Next run the following line to make IEADB available within the qPackage.
-qCreate::export_data(IEADB, database = "agreements", URL = "https://iea.uoregon.edu/base-agreement-list", package = "qEnviron")
+qCreate::export_data(IEADB, database = "agreements", URL = "https://iea.uoregon.edu/base-agreement-list")
 # This function also does two additional things.
 # First, it creates a set of tests for this object to ensure adherence to certain standards.
 # You can hit Cmd-Shift-T (Mac) or Ctrl-Shift-T (Windows) to run these tests locally at any point.
