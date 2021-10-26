d037e78c0c036367a075689b6188c2f0d240f54b
EstherPeev <esther.peev@graduateinstitute.ch>
2021-10-26 16:22:20 +0200

    Updated text database with new texts from ECOLEX and added comments in preparation script

---
+++ R/qEnviron-texts.R
@@ -0,0 +1,12 @@
+#' texts database documentation
+#'
+#' @format The texts database is a list that contains the
+#' following 1 datasets: AGR_TXT.
+#' For more information and references to each of the datasets used,
+#' please use the `data_source()`, `data_contrast()`, and `data_evolution()`
+#' functions.
+#'\describe{
+#' \item{AGR_TXT: }{A dataset with 6377 observations and the following
+#' 8 variables: qID_ref, Title, Beg, Text, Source, IEADB_ID, GNEVAR_ID, ECOLEX_ID.}
+#' }
+ "texts"
---
+++ data-raw/texts/AGR_TXT/prepare-AGR_TXT.R
@@ -0,0 +1,93 @@
+# ENVIRONMENTAL AGREEMENTS TEXTS script preparation
+
+# Due to the specificities of the text database,
+# the usual script preparation format has been adapted.
+
+# Step one: create a consolidated version of qEnviron agreements database
+AGR_TXT <- qData::consolidate(qEnviron::agreements, "any", "any")
+
+# Step two: extract treaty texts from IEADB website
+# This requires the original IEADB dataset with the IEADB_ID column
+IEADB_original <- readr::read_csv("data-raw/agreements/IEADB/treaties.csv")
+IEADB_original <- dplyr::as_tibble(IEADB_original) %>%
+  dplyr::rename(IEADB_ID = `IEA# (click for add'l info)`,
+                TreatyText = `Treaty Text`) %>% 
+  dplyr::select(IEADB_ID, TreatyText) %>% 
+  dplyr::filter(TreatyText == "Treaty Text**") %>% 
+  dplyr::arrange(IEADB_ID)
+IEADB_original$IEADB_ID <- as.character(IEADB_original$IEADB_ID)
+
+# Then, rvest package is used to web scrap IEADB treaty texts pages
+base <- "https://iea.uoregon.edu/treaty-text/"
+IEADB_original$TreatyText <- lapply(IEADB_original$IEADB_ID, function(s) tryCatch(rvest::read_html(paste0(base, s)) %>%
+                                                                                    rvest::html_nodes(".even") %>% 
+                                                                                    rvest::html_text(),error = function(e){as.character("Not found")} %>% 
+                                                                                    paste(collapse = ",")))
+# A source column is also added
+IEADB_original$Source <- lapply(IEADB_original$IEADB_ID, function(s) tryCatch(rvest::read_html(paste0(base, s)) %>%
+                                                                                rvest::html_nodes(".views-field-views-conditional") %>% 
+                                                                                rvest::html_text(),error = function(e){as.character("Not found")} %>% 
+                                                                                paste(collapse = ",")))
+
+# Step three: join IEADB text column with the consolidated version of
+# qEnviron 
+AGR_TXT <- dplyr::left_join(AGR_TXT, IEADB_original, by = "IEADB_ID")
+AGR_TXT <- as_tibble(AGR_TXT) %>% 
+  dplyr::select(qID, IEADB_ID, GNEVAR_ID, ECOLEX_ID, qID_ref, Title, Beg, TreatyText, Source)
+
+# Step four: complement the dataset with ECOLEX treaty texts (TO BE IMPROVED)
+# Filter observations with text = NULL and with an ECOLEX_ID
+ecolex_text <- AGR_TXT %>% 
+  dplyr::filter(!is.na(ECOLEX_ID) & TreatyText == "NULL")
+
+ecolex_text$ECOLEX_ID2 <- stringr::str_remove_all(ecolex_text$ECOLEX_ID, "-")
+
+# Use pdftools package to extract treaty texts
+base = "http://www.ecolex.org/server2neu.php/libcat/docs/TRE/Full/En/"
+ecolex_text$TreatyText <- lapply(ecolex_text$ECOLEX_ID, function(s) tryCatch(pdftools::pdf_text(paste0(base, s, ".pdf")), error = function(e){as.character("Not found")}))
+ecolex_text$TreatyTextb <- lapply(ecolex_text$ECOLEX_ID2, function(s) tryCatch(pdftools::pdf_text(paste0(base, s, ".pdf")), error = function(e){as.character("Not found")}))
+
+
+base2 = "http://www.ecolex.org/server2neu.php/libcat/docs/TRE/Full/Other/"
+ecolex_text$TreatyText2 <- lapply(ecolex_text$ECOLEX_ID, function(s) tryCatch(pdftools::pdf_text(paste0(base2, s, ".pdf")), error = function(e){as.character("Not found")}))
+ecolex_text$TreatyText2b <- lapply(ecolex_text$ECOLEX_ID2, function(s) tryCatch(pdftools::pdf_text(paste0(base2, s, ".pdf")), error = function(e){as.character("Not found")}))
+
+# base3 = "http://www.ecolex.org/server2neu.php/libcat/docs/TRE/Full/Fr/"
+# ecolex_text$TreatyText3 <- lapply(ecolex_text$ECOLEX_ID, function(s) tryCatch(pdftools::pdf_text(paste0(base3, s, ".pdf")), error = function(e){as.character("Not found")}))
+# ecolex_text$TreatyText3b <- lapply(ecolex_text$ECOLEX_ID2, function(s) tryCatch(pdftools::pdf_text(paste0(base3, s, ".pdf")), error = function(e){as.character("Not found")}))
+# 
+# base4 = "http://www.ecolex.org/server2neu.php/libcat/docs/TRE/Full/Sp/"
+# ecolex_text$TreatyText4 <- lapply(ecolex_text$ECOLEX_ID, function(s) tryCatch(pdftools::pdf_text(paste0(base4, s, ".pdf")), error = function(e){as.character("Not found")}))
+# ecolex_text$TreatyText4b <- lapply(ecolex_text$ECOLEX_ID2, function(s) tryCatch(pdftools::pdf_text(paste0(base4, s, ".pdf")), error = function(e){as.character("Not found")}))
+
+ecolex_text$TreatyText <- dplyr::na_if(ecolex_text$TreatyText, "Not found")
+ecolex_text$TreatyTextb <- dplyr::na_if(ecolex_text$TreatyTextb, "Not found")
+ecolex_text$TreatyText2 <- dplyr::na_if(ecolex_text$TreatyText2, "Not found")
+ecolex_text$TreatyText2b <- dplyr::na_if(ecolex_text$TreatyText2b, "Not found")
+
+ecolex_text$Text <- dplyr::coalesce(ecolex_text$TreatyText, ecolex_text$TreatyTextb, ecolex_text$TreatyText2,
+                                    ecolex_text$TreatyText2b)
+
+ecolex_text <- ecolex_text %>% 
+  dplyr::filter(!is.na(Text)) %>% 
+  dplyr::select(ECOLEX_ID, Text)
+
+ecolex_text$Source <- "ECOLEX"
+
+# Step five: join ECOLEX texts to the agreements dataset
+AGR_TXT <- dplyr::left_join(AGR_TXT,  ecolex_text, by = "ECOLEX_ID")
+AGR_TXT$Source.x <- dplyr::na_if(AGR_TXT$Source.x, "NULL")
+AGR_TXT$Source.y <- dplyr::na_if(AGR_TXT$Source.y, "NULL")
+
+AGR_TXT$TreatyText <- dplyr::na_if(AGR_TXT$TreatyText, "NULL")
+AGR_TXT$Text <- dplyr::na_if(AGR_TXT$Text, "NULL")
+AGR_TXT$Text <- dplyr::coalesce(AGR_TXT$TreatyText, AGR_TXT$Text)
+
+AGR_TXT <- dplyr::as_tibble(AGR_TXT) %>% 
+  dplyr::rename(Source = `Source.x`) %>% 
+  dplyr::select(qID_ref, Title, Beg, Text, Source, IEADB_ID, GNEVAR_ID, ECOLEX_ID)
+
+# Step six: export data into rda format 2 possible solutions
+qCreate::export_data(AGR_TXT, database = "texts", URL = "NA")
+# usethis::use_data(AGR_TXT, internal = T, overwrite = T)
+
