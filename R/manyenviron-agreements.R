21ca1529b705eadc59fd08b622c1292d014932d7
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-09-06 11:37:11 +0200

    Added HUGGO to agreements database

--- R/manyenviron-agreements.R
+++ R/manyenviron-agreements.R
@@ -1,24 +1,23 @@
 #' agreements database documentation
 #'
 #' @format The agreements database is a list that contains the
-#' following 5 datasets: IEADB, GNEVAR, ECOLEX, CIESIN, HEIDI.
+#' following 6 datasets: IEADB, GNEVAR, ECOLEX, CIESIN, HEIDI, HUGGO.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
+#' @imports manydata
 #'\describe{
 #' \item{IEADB: }{A dataset with 3666 observations and the following
-#' 10 variables: manyID, Title, Beg, DocType, AgreementType, Signature,
-#' Force, Lineage, treatyID, ieadbID.}
-#' \item{GNEVAR: }{A dataset with 7273 observations and the following
-#' 14 variables: manyID, Title, Beg, End, DocType, AgreementType, GeogArea,
-#' Signature, Force, Lineage, accessionC, accessionP, treatyID, gnevarID.}
+#' 10 variables: manyID, Title, Beg, DocType, AgreementType, Signature, Force, Lineage, treatyID, ieadbID.}
 #' \item{ECOLEX: }{A dataset with 2174 observations and the following
-#' 10 variables: manyID, Title, Beg, DocType, GeogArea, Signature, Force,
-#' Lineage, treatyID, ecolexID.}
+#' 10 variables: manyID, Title, Beg, DocType, GeogArea, Signature, Force, Lineage, treatyID, ecolexID.}
 #' \item{CIESIN: }{A dataset with 666 observations and the following
 #' 7 variables: manyID, Title, Beg, Signature, Force, Lineage, treatyID.}
 #' \item{HEIDI: }{A dataset with 2280 observations and the following
 #' 7 variables: manyID, Title, Beg, Signature, Lineage, treatyID, heidiID.}
+#' \item{HUGGO: }{A dataset with 8213 observations and the following
+#' 16 variables: manyID, Title, Beg, End, DocType, AgreementType, GeogArea, Signature, Force, Lineage, accessionC, accessionP, treatyID, gnevarID, TreatyText, Source.}
 #' }
+
 #'
 #' @details
 #' ``` {r, echo = FALSE, warning = FALSE}
