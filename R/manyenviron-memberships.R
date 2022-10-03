06c298ac562549e527722bc6fd53fdea3cfd01ad
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-10-03 17:53:39 +0200

    Added MEA_edges to memberships database

--- R/manyenviron-memberships.R
+++ R/manyenviron-memberships.R
@@ -1,7 +1,7 @@
 #' memberships database documentation
 #'
 #' @format The memberships database is a list that contains the
-#' following 5 datasets: ECOLEX_MEM, GNEVAR_MEM, IEADB_MEM, TFDD_MEM, HUGGO_MEM.
+#' following 4 datasets: ECOLEX_MEM, IEADB_MEM, TFDD_MEM, HUGGO_MEM.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
@@ -14,9 +14,13 @@
 #' \item{TFDD_MEM: }{A dataset with 2118 observations and the following
 #' 8 variables: manyID, CountryID, Title, Beg, Signature, tfddID, Memberships,
 #' treatyID.}
-#' \item{HUGGO_MEM: }{A dataset with 35671 observations and the following
-#' 13 variables: manyID, CountryID, Title, Beg, End, SignatureCountry,
-#' Signature, Rat, Force, Term, Withdrawal, gnevarID, treatyID.}
+#' \item{HUGGO_MEM: }{A dataset with 76954 observations and the following
+#' 31 variables: manyID, treatyID, CountryID, Title, Beg, End, Signature,
+#' Force, CountrySignature, CountryRat, CountryForce, CountryForce2,
+#' CountryForce3, Term, CountryWithdrawal, CountryWithdrawal2, gengID,
+#' ecolexID, ieaID, Comments, Deposit, obsolete, ProvisionalApp,
+#' Reservation, verified, Notes, CountryForce_ecolex, CountryForce_iea,
+#' Consent, Acceptance, Accession.}
 #' }
 
 #'
