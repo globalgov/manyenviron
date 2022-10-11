5038f62a582bdf427b71109909a85905b41d5884
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-10-11 14:59:04 +0200

    Updated tests for HUGGO memberships

--- tests/testthat/test_HUGGO_MEM.R
+++ tests/testthat/test_HUGGO_MEM.R
@@ -18,7 +18,7 @@ test_that("datasets have the required variables", {
                                 pointblank::vars(Beg))
 })
 
-# Date columns should be in messydt class
+# Date columns should be in mdate class
 test_that("Columns are not in date, POSIXct or POSIXlt class", {
   expect_false(any(lubridate::is.Date(memberships[["HUGGO_MEM"]])))
   expect_false(any(lubridate::is.POSIXct(memberships[["HUGGO_MEM"]])))
