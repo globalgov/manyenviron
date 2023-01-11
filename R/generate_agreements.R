cb0c3663addf211631c799733798b56d434f1c02
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-01-11 17:19:36 -0300

    Updated generate_agreements() and generate_memberships() by making these follow a proportion of bilateral and multilateral agreements in HUGGO. Users can also select a treaty type, if desired.

--- R/generate_agreements.R
+++ R/generate_agreements.R
@@ -2,16 +2,24 @@
 #' 
 #' @description Generate a list of fictional agreements and memberships.
 #' @param n Integer number of fictional agreement names to generate.
+#' @param treaty_type What type of treaty would you likebe returned?
+#' By default, "any" treaty.
+#' Other options include "bilaterals" and "multilaterals" 
 #' @return String vector of fictional agreement names and/or memberships.
 #' @name generate_
 
 #' @rdname generate_
 #' @examples
 #' generate_agreements(12)
+#' generate_agreements(12, treaty_type = "bilaterals")
+#' generate_agreements(12, treaty_type = "multilaterals")
 #' @export
-generate_agreements <- function(n = 10){
+generate_agreements <- function(n = 10, treaty_type = "any"){
+  partylib <- trimws(paste(rep(c("Between The Parties of "), n),
+                           sample(countryregex$Label, n, replace = TRUE),
+                           rep(c(" and "), n),
+                           sample(countryregex$Label, n, replace = TRUE)))
   typelib <- c("Agreement", "Convention")
-  # partylib <- c("Between The Parties")
   subjlib <- c("On", "For", "Concerning", "Limiting",
                "Regulating", "Regarding", "Relating To",
                "Cooperation In",
@@ -40,25 +48,64 @@ generate_agreements <- function(n = 10){
                  "The Development Of Geothermal Energy",
                  "Energy Research And Development",
                  "Shrimp")
-  trimws(paste(sample(typelib, n, replace = TRUE),
-               sample(subjlib, n, replace = TRUE),
-               sample(targetlib, n, replace = TRUE)))
+  if (treaty_type == "bilaterals") {
+    out <- trimws(paste(sample(typelib, n, replace = TRUE),
+                        partylib,
+                        sample(subjlib, n, replace = TRUE),
+                        sample(targetlib, n, replace = TRUE)))
+  } else if (treaty_type == "multilaterals") {
+    out <- trimws(paste(sample(typelib, n, replace = TRUE),
+                        sample(subjlib, n, replace = TRUE),
+                        sample(targetlib, n, replace = TRUE)))
+  } else {
+    bilaterals <- round(sum(grepl("between", agreements$HUGGO$Title,
+                                  ignore.case = TRUE)) / length(agreements$HUGGO$Title),
+                        digits = 1)
+    partylib[sample(n*bilaterals)] <- ""
+    out <- trimws(paste(sample(typelib, n, replace = TRUE),
+                        partylib,
+                        sample(subjlib, n, replace = TRUE),
+                        sample(targetlib, n, replace = TRUE)))
+  }
+  out
 }
 
 #' @rdname generate_
+#' @param list Would you like members to be listed in one observation?
+#' By default, FALSE.
 #' @examples
 #' generate_memberships(12)
+#' generate_memberships(12, treaty_type = "bilaterals")
+#' generate_memberships(12, treaty_type = "multilaterals")
+#' generate_memberships(12, list = TRUE)
 #' @export
-generate_memberships <- function(n = 10) {
+generate_memberships <- function(n = 10, treaty_type = "any", list = FALSE) {
   membs <- countryregex$StatID
-  agreements <- data.frame(title = generate_agreements(n = n),
+  g_agreements <- data.frame(title = generate_agreements(n = n, treaty_type =
+                                                         treaty_type),
                            nm = sample(100, n), membs = 0)
-  for (k in seq_len(length(agreements$nm))) {
-    agreements$membs[k] <- trimws(paste(sample(membs, as.numeric(agreements$nm[k])),
-                                        collapse = ", "))
+  coment <- sapply(countryregex[, 3], function(x) grepl(x, g_agreements$title,
+                                                        ignore.case = T,
+                                                        perl = T) * 1)
+  colnames(coment) <- countryregex[, 1]
+  rownames(coment) <- g_agreements$title
+  out <- unname(apply(coment, 1, function(x) paste(names(x[x == 1]),
+                                                   collapse = ", ")))
+  out[out == ""] <- NA
+  for (k in seq_len(length(g_agreements$nm))) {
+    g_agreements$membs[k] <- ifelse(grepl("Between The Parties of",
+                                        g_agreements$title[k]),
+                                  out[k],
+                                  trimws(paste(sample(membs,
+                                                      as.numeric(g_agreements$nm[k])),
+                                               collapse = ", ")))
   }
-  agreements <- plyr::ddply(agreements, .(title), function(DF) {
-    data.frame(membership = trimws(strsplit(DF$membs, ",")[[1]]))
+  g_agreements <- dplyr::select(g_agreements, -nm) %>%
+    dplyr::rename(membership = membs)
+  if (list == FALSE) {
+    g_agreements <- plyr::ddply(g_agreements, .(title), function(DF) {
+      data.frame(membership = trimws(strsplit(DF$membership, ",")[[1]]))
     })
-  agreements
+  }
+  g_agreements
 }
