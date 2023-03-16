cc94e268354ad7c43d975dae2986f6be0b3e700e
Diego del Moral Suárez <delmoralsu@gmail.com>
2023-03-16 15:13:02 +0100

    Export HUGGO_new to HUGGO
    Correct mistake where 1 treaty was mistakenly removed. Split lines in many-environ-agreements.R

--- R/manyenviron-agreements.R
+++ R/manyenviron-agreements.R
@@ -6,17 +6,27 @@
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
 #' \item{IEADB: }{A dataset with 3666 observations and the following
-#' 10 variables: manyID, Title, Beg, DocType, AgreementType, Signature, Force, Lineage, treatyID, ieadbID.}
+#' 10 variables: manyID, Title, Beg, DocType, AgreementType, Signature, Force, 
+#' Lineage, treatyID, ieadbID.}
 #' \item{ECOLEX: }{A dataset with 2174 observations and the following
-#' 10 variables: manyID, Title, Beg, DocType, GeogArea, Signature, Force, Lineage, treatyID, ecolexID.}
+#' 10 variables: manyID, Title, Beg, DocType, GeogArea, Signature, Force, 
+#' Lineage, treatyID, ecolexID.}
 #' \item{CIESIN: }{A dataset with 666 observations and the following
 #' 7 variables: manyID, Title, Beg, Signature, Force, Lineage, treatyID.}
 #' \item{HEIDI: }{A dataset with 2280 observations and the following
 #' 7 variables: manyID, Title, Beg, Signature, Lineage, treatyID, heidiID.}
-#' \item{HUGGO: }{A dataset with 4084 observations and the following
-#' 48 variables: manyID, Title, Beg, End, Signature, Force, url, AgreementType, DocType, GeogArea, gengID, ieaID, ecolexID, treatyID, Parties, verified, DocValidUntilDate, Notes, Download, MEA_type, Ambit, Region, subject_ecolex, subject_iea, Keywords, Lineage, Sequence, AdoptedIn, Languages, Appendices, Depository, DepositoryURL, Published, Abstract, Website1, Website2, Secretariat, SecretariatURL, UNEP, Supersedes, References, EnabledBy, AmendedBy, Lit, Data, Coded, TreatyText, Language.}
+#' \item{HUGGO: }{A dataset with 4085 observations and the following
+#' 48 variables: manyID, Title, Beg, End, Signature, Force, url, AgreementType,
+#' DocType, GeogArea, gengID, ieaID, ecolexID, treatyID, Parties, verified,
+#' DocValidUntilDate, Notes, Download, MEA_type, Ambit, Region, subject_ecolex,
+#' subject_iea, Keywords, Lineage, Sequence, AdoptedIn, Languages, Appendices,
+#' Depository, DepositoryURL, Published, Website1, Website2, Secretariat,
+#' SecretariatURL, UNEP, Supersedes, References, EnabledBy, AmendedBy, Lit,
+#' Data, Coded, Abstract, TreatyText, Language.}
 #' \item{AIGGO: }{A dataset with 3815 observations and the following
-#' 13 variables: manyID, treatyID, Title, Beg, End, Signature, Force, action_area, linkage, accessionC, accessionP, termination_type, termination_date.}
+#' 13 variables: manyID, treatyID, Title, Beg, End, Signature, Force,
+#' action_area, linkage, accessionC, accessionP, termination_type,
+#' termination_date.}
 #' }
 
 #'
