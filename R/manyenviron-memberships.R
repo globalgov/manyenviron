ca195967e6ceef063b7a895c4348fa0656c76376
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-04-04 14:11:09 +0200

    Updated titles and IDs in memberships database

--- R/manyenviron-memberships.R
+++ R/manyenviron-memberships.R
@@ -5,17 +5,17 @@
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
-#' \item{ECOLEX_MEM: }{A dataset with 25003 observations and the following
-#' 10 variables: manyID, stateID, Title, Beg, End, stateSignature, Force, Rat,
-#' ecolexID, treatyID.}
+#' \item{ECOLEX_MEM: }{A dataset with 24975 observations and the following
+#' 10 variables: manyID, treatyID, Title, Beg, stateID, End, stateSignature,
+#' Force, Rat, ecolexID.}
 #' \item{IEADB_MEM: }{A dataset with 15463 observations and the following
-#' 12 variables: manyID, stateID, Title, Beg, End, stateSignature, Signature,
-#' Rat, Force, DocType, ieadbID, treatyID.}
-#' \item{TFDD_MEM: }{A dataset with 2118 observations and the following
-#' 8 variables: manyID, stateID, Title, Beg, Signature, tfddID, Memberships,
-#' treatyID.}
+#' 12 variables: manyID, treatyID, Title, Beg, stateID, End, stateSignature,
+#' Signature, Rat, Force, DocType, ieadbID.}
+#' \item{TFDD_MEM: }{A dataset with 1832 observations and the following
+#' 8 variables: manyID, treatyID, Title, Beg, stateID, Signature, tfddID,
+#' Memberships.}
 #' \item{HUGGO_MEM: }{A dataset with 52462 observations and the following
-#' 31 variables: manyID, treatyID, stateID, Title, Beg, End, Signature, Force,
+#' 31 variables: manyID, treatyID, Title, Beg, stateID, End, Signature, Force,
 #' stateSignature, stateRat, stateForce, stateForce2, stateForce3, Term,
 #' stateWithdrawal, stateWithdrawal2, gengID, ecolexID, ieaID, Comments,
 #' Deposit, obsolete, ProvisionalApp, Reservation, verified, Notes,
