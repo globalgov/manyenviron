b940e78f755daae5bca38d4f8ac193b3dd406186
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-07-28 16:47:33 +0200

    Added package startup message

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onAttach <- function(lib, pkg) {
+  msg <- c(paste0("manyenviron ", utils::packageVersion("manyenviron")),
+           "\nFor more information about the package pleae visit https://globalgov.github.io/manyenviron/",
+           "\nType 'citation(\"manyenviron\")' for citing this R package in publications.")
+  packageStartupMessage(msg)      
+  invisible()
+}
